c295cad8ea8e464bf3e4972331dacd471df288d4
Subashini2305 <subasrinivasan2005@gmail.com>
2026-09-22 14:29:13 +0530

    Initial Flask application

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return """
+    <h1>Hello from my automated EC2 setup! 🚀</h1>
+    <p>This application was deployed using EC2 automation.</p>
+    """
+
+@app.route("/about")
+def about():
+    return """
+    <h1>About</h1>
+    <p>This is my new AWS DevOps project.</p>
+    """
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
