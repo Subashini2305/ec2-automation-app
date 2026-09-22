26c1011864a227365a78159c1c3c53d273f9ce84
Subashini2305 <subasrinivasan2005@gmail.com>
2026-09-22 17:15:31 +0530

    Add hello route

--- app.py
+++ app.py
@@ -15,6 +15,12 @@ def about():
     <h1>About</h1>
     <p>This is my new AWS DevOps project.</p>
     """
+@app.route("/hello")
+def hello():
+    return """
+    <h1>Hello from GitHub Actions! 🚀</h1>
+    <p>This page was deployed automatically.</p>
+    """
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
